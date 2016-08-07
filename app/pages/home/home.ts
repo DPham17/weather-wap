8f67df271d8549261155fd503eb0e8b3babb8a7f
Dzu Pham <dtptw6@mst.edu>
2016-08-07 12:03:02 -0500

    added Weather api url

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -13,20 +13,28 @@ import {calendarModel} from '../../models/calendarModel'
 export class HomePage {
 
   recievedData: any;
+  recievedWeather: any;
   email_1: string = 'dzutpham@gmail.com';
-  apiKey: string = 'AIzaSyBu3Igz5qg8o8w56kcRNlpc-EA4xNvyVKY';
+  apiKeyCal: string = 'AIzaSyAT85ggW19Y2_4i2bGPZ_O2hBTcN94wBHY';
+  apiKeyW: string = '61e9b5e1f5096d77dbd8dc5faf674e43';
   timeMin: string = '2016-08-04T19:36:34+00:00';
   maxEvents: string = '10';
+  city: string = 'bentonville,ar'
 
   items: Array<calendarModel> = new Array<calendarModel>();
 
-  // Gotta build this url string
-  urlCalendar: string = 'https://www.googleapis.com/calendar/v3/calendars/' + this.email_1 + '/events?maxResults=' + this.maxEvents + '&timeMin=' + this.timeMin + '&key=' + this.apiKey;
+  // Gotta build url string
+  urlCalendar: string = 'https://www.googleapis.com/calendar/v3/calendars/' + this.email_1 + '/events?' + 'key=' + this.apiKeyCal;
 
+  //https://www.googleapis.com/calendar/v3/calendars/dzutpham@gmail.com/events?maxResults=10&timeMin=2016-08-04T00:12:43+00:00&key=AIzaSyD6u2G4mYR2EmEcBc5R2L0ciVYbr20L0eg
+
+  urlWeather: string = 'http://api.openweathermap.org/data/2.5/weather?q=' + this.city + '&appid=' + this.apiKeyW;
 
   constructor(private nav: NavController, private http: Http, public events: Events) {
     console.log(this.urlCalendar);
-    this.getData();
+    // Calls the Google Calendar API
+    //this.getData();
+    this.getWeather();
   }
 
   tapSandbox(event, item) {
@@ -61,6 +69,24 @@ export class HomePage {
     }
     this.events.publish('calendar:datapullFinished');
   }
+
+  getWeather() {
+    this.http.get(this.urlWeather).subscribe(
+      recievedWeather => { //convert the response to a json object, and map this object to recieved data
+        console.log(recievedWeather);
+        this.recievedWeather = recievedWeather;
+        //this.processRecievedData();
+        //this.sqlStorageService.storeData('coolerFallback', recievedData); //set the fallback cache to contain this data we just pulled
+      },
+      (err) => { //if the remote pull fails for any reason
+        console.log('Error encountered fetching data from remote: \n' + err);
+        //console.log('Using fallback cache...');
+        //this.offlineFallback();
+      }
+    );
+    console.log(this.recievedWeather);
+  }
+
 
 
 }
