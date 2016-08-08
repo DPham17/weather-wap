7811819ae7d9b40a7d1bd2715a516de4612681e6
Dzu Pham <dtptw6@mst.edu>
2016-08-07 20:06:22 -0500

    got calendar api to pull data

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -17,23 +17,24 @@ export class HomePage {
   email_1: string = 'dzutpham@gmail.com';
   apiKeyCal: string = 'AIzaSyAT85ggW19Y2_4i2bGPZ_O2hBTcN94wBHY';
   apiKeyW: string = '61e9b5e1f5096d77dbd8dc5faf674e43';
-  timeMin: string = '2016-08-04T19:36:34+00:00';
+  timeMin: string = '2016-08-04T19%3A36%3A34%2B00%3A00';
+  //timeMin: string = '2016-08-04T19:36:34+00:00';
   maxEvents: string = '10';
   city: string = 'bentonville,ar'
 
   items: Array<calendarModel> = new Array<calendarModel>();
 
   // Gotta build url string
-  urlCalendar: string = 'https://www.googleapis.com/calendar/v3/calendars/' + this.email_1 + '/events?' + 'key=' + this.apiKeyCal;
+  urlCalendar: string = 'https://www.googleapis.com/calendar/v3/calendars/' + this.email_1 + '/events?maxResults=10&timeMin=' + this.timeMin + '&key=' + this.apiKeyCal;
 
   //https://www.googleapis.com/calendar/v3/calendars/dzutpham@gmail.com/events?maxResults=10&timeMin=2016-08-04T00:12:43+00:00&key=AIzaSyD6u2G4mYR2EmEcBc5R2L0ciVYbr20L0eg
 
-  urlWeather: string = 'http://api.openweathermap.org/data/2.5/weather?q=' + this.city + '&appid=' + this.apiKeyW;
+  urlWeatherCurrent: string = 'http://api.openweathermap.org/data/2.5/weather?q=' + this.city + '&units=imperial&appid=' + this.apiKeyW;
 
   constructor(private nav: NavController, private http: Http, public events: Events) {
     console.log(this.urlCalendar);
     // Calls the Google Calendar API
-    //this.getData();
+    this.getData();
     this.getWeather();
   }
 
@@ -57,7 +58,7 @@ export class HomePage {
         //this.offlineFallback();
       }
     );
-    console.log(this.recievedData);
+    console.log("Calendar output " + this.recievedData);
   }
 
   private processRecievedData() {
@@ -71,7 +72,7 @@ export class HomePage {
   }
 
   getWeather() {
-    this.http.get(this.urlWeather).subscribe(
+    this.http.get(this.urlWeatherCurrent).subscribe(
       recievedWeather => { //convert the response to a json object, and map this object to recieved data
         console.log(recievedWeather);
         this.recievedWeather = recievedWeather;
