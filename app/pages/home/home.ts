3c17b86d1924c15094f3d608da750f2f4cb26255
Dzu Pham <dpham2@homeoffice.wal-mart.com>
2016-08-09 20:26:46 -0500

    gotten current weather data to work, Also doc for old code

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -5,6 +5,7 @@ import {Events} from 'ionic-angular';
 import {NavController} from 'ionic-angular';
 import {SandboxPage} from '../sandbox/sandbox'
 import {calendarModel} from '../../models/calendarModel'
+import {weatherModel} from '../../models/weatherModel'
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
@@ -13,7 +14,6 @@ import {calendarModel} from '../../models/calendarModel'
 export class HomePage {
 
   recievedData: any;
-  summary: any;
   recievedWeather: any;
   email_1: string = 'dzutpham@gmail.com';
   apiKeyCal: string = 'AIzaSyAT85ggW19Y2_4i2bGPZ_O2hBTcN94wBHY';
@@ -21,24 +21,23 @@ export class HomePage {
   timeMin: string = '2016-08-04T19%3A36%3A34%2B00%3A00';
   //timeMin: string = '2016-08-04T19:36:34+00:00';
   maxEvents: string = '10';
-  city: string = 'bentonville,ar'
+  city: string = 'Bentonville,US'
 
   calendarItems: Array<calendarModel> = new Array<calendarModel>();
+  currentWeather: Array<weatherModel> = new Array<weatherModel>();
 
   // Gotta build url string
   urlCalendar: string = 'https://www.googleapis.com/calendar/v3/calendars/' + this.email_1 + '/events?maxResults=10&timeMin=' + this.timeMin + '&key=' + this.apiKeyCal;
 
-  //https://www.googleapis.com/calendar/v3/calendars/dzutpham@gmail.com/events?maxResults=10&timeMin=2016-08-04T00:12:43+00:00&key=AIzaSyD6u2G4mYR2EmEcBc5R2L0ciVYbr20L0eg
-
   urlWeatherCurrent: string = 'http://api.openweathermap.org/data/2.5/weather?q=' + this.city + '&units=imperial&appid=' + this.apiKeyW;
+
+  urlForecast: string = 'http://api.openweathermap.org/data/2.5/forecast?q=' + this.city + '&units=imperial&appid=' + this.apiKeyW;
 
   constructor(private nav: NavController, private http: Http, public events: Events) {
     // Calls the Google Calendar API
     this.getData();
     this.getWeather();
-
-    console.log("Test2 ");
-    console.log(this.recievedData);
+    //this.getForecast();
   }
 
   tapSandbox(event, item) {
@@ -47,22 +46,21 @@ export class HomePage {
     });
   }
 
+  logError(err) {
+    console.error("There was an error: " + err);
+  }
+
   getData() {
     this.http.get(this.urlCalendar)
       .map(res => res.json())
       .subscribe(
         data => {
           this.recievedData = data;
-          this.summary = data.summary;
           this.processRecievedData(data);
         },
         err => this.logError(err),
         () => console.log('Calendar data complete')
       );
-  }
-
-  logError(err) {
-    console.error("There was an error: " + err);
   }
 
   /*
@@ -99,7 +97,7 @@ export class HomePage {
   */
 
   private processRecievedData(data) {
-    console.log(data);
+    console.log(data); // This is only to show the layout of the data
     let model: calendarModel = new calendarModel();
     model.initialize(this.recievedData);
     this.calendarItems.push(model);
@@ -132,7 +130,29 @@ private processRecievedData() {
 
 
 
+  // This is the current forcast
+  getWeather() {
+    this.http.get(this.urlWeatherCurrent)
+    .map(res => res.json())
+    .subscribe(
+      data => {
+        this.recievedWeather = data;
+        this.processWeatherData(data);
+      },
+      err => this.logError(err),
+      () => console.log('Current weather data recieved')
+    );
+  }
 
+  private processWeatherData(data) {
+    console.log(data); // This is only to show the layout of the data
+    let model: weatherModel = new weatherModel();
+    model.initialize(this.recievedWeather);
+    this.currentWeather.push(model);
+    console.log(this.currentWeather);
+  }
+
+  /* Reference to the offline cache
   getWeather() {
     this.http.get(this.urlWeatherCurrent).subscribe(
       recievedWeather => { //convert the response to a json object, and map this object to recieved data
@@ -148,7 +168,7 @@ private processRecievedData() {
       }
     );
   }
-
+  */
 
 
 }
